c4291a26f50d583e71d573b554a3d6d1c86c92f7
Cameron Keith <cameron.s.keith.26@dartmouth.edu>
2024-05-10 21:17:59 -0400

    updated files for Docker pipeline script

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,3 +1,4 @@
+# type: ignore
 import torch
 import av
 import torchvision
@@ -5,30 +6,66 @@ from torch import nn
 from torchvision import transforms
 from torchvision.datasets import UCF101
 import nbformat
-from nbconvert import HTMLExporter
+from nbconvert.preprocessors import ExecutePreprocessor
 import traitlets
+import requests
+import patoolib
+import papermill as pm
 
 class setup():
-
-    models = []
+    # choose models to train from scratch
+    models = ["../training_files/base_model.ipynb", 
+              "../training_files/base_outlier_model.ipynb"] # add more models here
+    
+    data_files = []
+    train_samples = -1
 
     def __init__(self, models):
         models = models
 
-    def load_model(model_path):
+    # execute
+    pipeline()
+
+    def pipeline():
+        # load the data
+        load_raw_data()
+        load_data()
+        # load the models
+        for model in models:
+            train_model(model)
+            with open('../results/results.txt', 'r') as f:
+                print(f.read())
+
+    def train_model(model_path):
         # Load the notebook
-        notebook = nbformat.read(model_path, as_version=4)
-
-        # Configure the exporter
-        html_exporter = HTMLExporter()
-        html_exporter.template_file = 'basic' # Use the basic template
-
-        # Execute the notebook
-        ep = HTMLExporter(traitlets.config.Config())
-        (body, resources) = ep.from_notebook_node(notebook)
+        pm.execute_notebook(
+            input_path = model_path,
+            output_path = f"{model_path.split('.')[0]}_executed.ipynb",
+            parameters = [self.data_files, train_samples]
+        )
 
     def load_raw_data():
-        pass
+        # get videos
+        url = "https://www.crcv.ucf.edu/datasets/human-actions/ucf101/UCF101.rar"
+        response = requests.get(url, stream=True)
+
+        with open("../data/UCF-101.rar", "wb") as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+
+        # Unrar the downloaded file
+        patoolib.extract_archive("../data/UCF-101.rar", outdir="../data/UCF-101")
+
+        # get labels
+        url = "https://www.crcv.ucf.edu/wp-content/uploads/2019/06/Datasets_UCF101-VideoLevel.zip"
+        response = requests.get(url, stream=True)
+
+        with open("../data/ucfTrainTestlist.zip", "wb") as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+
+        # Unrar the downloaded file
+        patoolib.extract_archive("../data/ucfTrainTestlist.zip", outdir="../data/ucfTrainTestlist")
 
     def load_data(train_size = 0.1, train_test_split = 0.8, batch_size = 128):
         frames_per_clip = 5
@@ -54,12 +91,6 @@ class setup():
             transforms.Lambda(interpolate),
         ])
 
-        def custom_collate(batch):
-            filtered_batch = []
-            for video, _, label in batch:
-                filtered_batch.append((video, label))
-            return torch.utils.data.dataloader.default_collate(filtered_batch)
-
         # load train test datasets
         train_dataset = UCF101(ucf_data_dir, ucf_label_dir, frames_per_clip=frames_per_clip,
                             step_between_clips=step_between_clips, train=True, transform=tfs)
@@ -75,6 +106,8 @@ class setup():
 
         train_set, test_set_org = torch.utils.data.random_split(train_dataset, [train_size, train_size_rest])
         test_set, test_set2 = torch.utils.data.random_split(test_dataset, [test_size, test_size_rest])
+
+        self.train_samples = round(train_size/1000)
 
         with open('../data/train_dataset.pt', 'wb') as f:
             torch.save(train_dataset, f)
@@ -93,40 +126,15 @@ class setup():
         with open(f'../data/train_dataset_{round(train_size_rest/1000)}k_rest.pt', 'wb') as f:
             torch.save(test_set_org, f)
 
+        self.files.append(f'../data/train_dataset_{round(train_size/1000)}k.pt', 
+                     f'../data/test_dataset_{round(test_size/1000)}k.pt', 
+                     f'../data/train_dataset_{round(train_size_rest/1000)}k_rest.pt',
+                     f'../data/test_dataset_{round(test_size_rest/1000)}k_rest.pt', 
+                     '../data/train_dataset.pt', 
+                     '../data/test_dataset.pt')
+
         # Save test_dataset
         with open(f'../data/test_dataset_{round(test_size_rest/1000)}k_rest.pt', 'wb') as f:
             torch.save(test_set2, f)
 
-        # Load train_dataset
-        with open('../data/train_dataset.pt', 'rb') as f:
-            train_dataset = torch.load(f)
-
-        # Load test_dataset
-        with open('../data/test_dataset.pt', 'rb') as f:
-            test_dataset = torch.load(f)
-
-        # Load train_dataset_100k
-        with open('../data/train_dataset_100k.pt', 'rb') as f:
-            train_set = torch.load(f)
-
-        # Load test_dataset_20k
-        with open('../data/test_dataset_20k.pt', 'rb') as f:
-            test_set = torch.load(f)
-
-        # Load train_dataset_100k_rest
-        with open('../data/train_dataset_100k_rest.pt', 'rb') as f:
-            test_set_org = torch.load(f)
-
-        # Load test_dataset_20k_rest
-        with open('../data/test_dataset_20k_rest.pt', 'rb') as f:
-            test_set2 = torch.load(f)
-            
-        train_loader_sub = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True,
-                                                collate_fn=custom_collate)
-        test_loader_sub = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True,
-                                                collate_fn=custom_collate)
-        train_loader_full = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                                collate_fn=custom_collate)
-        test_loader_full = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True,
-                                                collate_fn=custom_collate)
         return (train_loader_sub, test_loader_sub, train_loader_full, test_loader_full)
